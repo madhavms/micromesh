111ae546e21d5c9bb3b49a536b899b7517fdfe25
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-11-16 13:52:31 +0000

    API for stock data

--- finAPI/stockapi/main.py
+++ finAPI/stockapi/main.py
@@ -3,6 +3,8 @@ from redis_om import get_redis_connection, HashModel
 from fastapi.middleware.cors import CORSMiddleware
 from pathlib import Path
 import os
+import requests
+import json
 
 from utils import global_var
 
@@ -20,11 +22,22 @@ app.add_middleware(
     allow_headers=['*']
 )
 
-class StockDetail(HashModel):
-    id:str
-    name:str
-    stock_exchange:str
-    open:float
-    high:float
-    low:float
-    last:float
+@app.get("/")
+async def root():
+    return {"message": "Welcome to StockAPI"}
+
+@app.get('/stocks')
+def get_all_stocks():
+    res = requests.get(global_var.STOCK_DATA_URL)
+    stock_data = json.loads(res.text)
+    return stock_data
+
+
+@app.get('/stocks/{id}')
+def get_stock(id):
+    res = requests.get(global_var.STOCK_DATA_URL)
+    response = json.loads(res.text)
+    for stock_data in response:
+        if stock_data['id'] == id:
+            return stock_data
+    return {}
