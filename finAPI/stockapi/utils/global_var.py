695f613cf949a9e61da9944b000c435f8a1c2ac4
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-11-16 14:03:51 +0000

    Updated path for stock data

--- finAPI/stockapi/utils/global_var.py
+++ finAPI/stockapi/utils/global_var.py
@@ -1,1 +1,1 @@
-STOCK_DATA_URL = 'https://gist.githubusercontent.com/madhavms/5f9f5323076ddc25a5b687c2831338bc/raw/344d6c88dff05eb37f735cb69694ba3f5e5f679c/dummyStockData.json'
+STOCK_DATA_PATH = './utils/stock_data.json'
