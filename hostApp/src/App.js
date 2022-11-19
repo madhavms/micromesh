642b891f5f911eb0f915ba40c130121936ec41a7
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-11-19 14:34:58 +0000

    Dynamically add widgets to screen

--- hostApp/src/App.js
+++ hostApp/src/App.js
@@ -1,11 +1,48 @@
 const StockWidget = React.lazy(() => import("finWidget/StockWidget"));
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import "./styles.css";
 
+
 const App = () => {
+  const [stockList, setStockList] = useState([]);
+
+  console.log(stockList)
+
+  const [allStockList, setAllStockList] = useState([])
+
+  const handleChange = (e) => {
+    e.preventDefault()
+    const id = e.target.value;
+    const stock = allStockList.filter(stock => stock.id === id)
+    setStockList(prev => [...prev, ...stock])
+  }
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8000/stocklist')
+      .then(response => {
+        if (!response.data) {
+          return;
+        }
+        const stockData = response.data;
+        setAllStockList(stockData);
+        console.log(stockList)
+      })
+  }, [])
+
   return (
     <div>
       <h1 className="text-center">Host Application</h1>
+      <select onChange = {handleChange} style = {{marginLeft:250}}>
+      {allStockList.map(stock => <option value = {stock.id} key={stock.id}>{stock.name}</option>)}
+      </select>
+      &nbsp;
+      {stockList.map((widget) => (
+        <React.Suspense fallback="...loading">
+        <StockWidget symbol={widget.id} />
+        </React.Suspense>
+      ))}
     </div>
   );
 };
