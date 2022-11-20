78daab9a444a14d30722d5a0666ebebec2ea8d85
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-11-20 14:08:23 +0000

    Added widget delete feature

--- hostApp/src/App.js
+++ hostApp/src/App.js
@@ -3,48 +3,62 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import "./styles.css";
 
-
 const App = () => {
   const [stockList, setStockList] = useState([]);
 
-  console.log(stockList)
+  const [allStockList, setAllStockList] = useState([]);
 
-  const [allStockList, setAllStockList] = useState([])
+  const stockFilter = (stock) => {
+    let list = stockList.map((listItem) => listItem.id);
+    return !list.includes(stock.id);
+  };
+
+  const handleDelete = (e) => {
+    let id = e.target.getAttribute('value');
+    console.log('hi',id)
+    setStockList([...stockList.filter((stock) => stock.id !== id)]);
+  };
 
   const handleChange = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const id = e.target.value;
-    const stock = allStockList.filter(stock => stock.id === id)
-    setStockList(prev => [...prev, ...stock])
-  }
+    const stock = allStockList.filter((stock) => stock.id === id);
+    setStockList((prev) => [...prev, ...stock]);
+  };
 
   useEffect(() => {
-    axios
-      .get('http://localhost:8000/stocklist')
-      .then(response => {
-        if (!response.data) {
-          return;
-        }
-        const stockData = response.data;
-        setAllStockList(stockData);
-        console.log(stockList)
-      })
-  }, [])
+    axios.get("http://localhost:8000/stocklist").then((response) => {
+      if (!response.data) {
+        return;
+      }
+      const stockData = response.data;
+      setAllStockList(stockData);
+    });
+  }, []);
 
   return (
     <div className="container">
       <h1 className="text-center">Host Application</h1>
-      <select onChange = {handleChange} className="select">
-      <option value="" selected disabled >Select a stock</option>
-      {allStockList.map(stock => <option value = {stock.id} key={stock.id}>{stock.name}</option>)}
+      <select onChange={handleChange} className="select" value="">
+        <option key="empty" value="" disabled>
+          Select a stock
+        </option>
+        {allStockList.filter(stockFilter).map((stock) => (
+          <option value={stock.id} key={stock.id}>
+            {stock.name}
+          </option>
+        ))}
       </select>
       &nbsp;
       {stockList.map((widget) => (
-        <React.Suspense fallback="...loading">
-        <div>
-        <StockWidget symbol={widget.id} />
-        </div>  
-        </React.Suspense>
+        <div key={widget.id} className="flex-container">
+          <React.Suspense fallback="...loading">
+            <StockWidget symbol={widget.id} />
+            <div >
+            <img value={widget.id} onClick={handleDelete} className="image" src="https://gist.githubusercontent.com/madhavms/8cb87494048689fe98177ed2bb6ba329/raw/4d5b97da61310840957cf83fc101004f117a9947/trashcan.svg"></img>
+            </div>
+          </React.Suspense>
+        </div>
       ))}
     </div>
   );
