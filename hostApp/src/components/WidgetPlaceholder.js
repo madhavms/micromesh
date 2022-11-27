a39f4032030be72a464b1c64e0f48ee3038cb1c6
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-11-27 06:14:31 +0000

    Update WidgetPlaceholder.js

--- hostApp/src/components/WidgetPlaceholder.js
+++ hostApp/src/components/WidgetPlaceholder.js
@@ -1,36 +1,30 @@
-import React from 'react';
+import React from "react";
 import moment from "moment";
-export const WidgetPlaceholder = () => 
-(<div draggable className="flex">
-<Template/>
-<img
-className="image"
-src="https://gist.githubusercontent.com/madhavms/8cb87494048689fe98177ed2bb6ba329/raw/4d5b97da61310840957cf83fc101004f117a9947/trashcan.svg"
-></img>
-</div>)
+export const WidgetPlaceholder = () => (
+  <div draggable className="flex">
+    <Template />
+    <img
+      className="image"
+      src="https://gist.githubusercontent.com/madhavms/8cb87494048689fe98177ed2bb6ba329/raw/4d5b97da61310840957cf83fc101004f117a9947/trashcan.svg"
+    ></img>
+  </div>
+);
 
-const Template = () => 
-{  
-     const varColor = "text-green-500";
-    return(
-        <div>
-        <div
-        className={
-          "quote rounded-lg shadow-md p-4 bg-gray-800 w-64 h-52px"
-        }
-      >
-        <span className={"quoteSymbol text-sm text-white font-bold"}>
-        ---
+const Template = () => {
+  const varColor = "text-green-500";
+  return (
+    <div>
+      <div className={"quote rounded-lg shadow-md p-4 bg-gray-800 w-64 h-52px"}>
+        <span className={"quoteSymbol text-sm text-white font-bold"}>---</span>
+        <span
+          className={"quoteSymbol ui-sans-serif text-2xs text-gray-400 ml-1"}
+        >
+          -----
         </span>
         <span
           className={"quoteSymbol ui-sans-serif text-2xs text-gray-400 ml-1"}
         >
-        -----
-        </span>
-        <span
-          className={"quoteSymbol ui-sans-serif text-2xs text-gray-400 ml-1"}
-        >
-        (---)
+          (---)
         </span>
         <div className={"quote flex flex-row justify-between mt-1"}>
           <div
@@ -47,11 +41,11 @@ const Template = () =>
                 "quoteTime ui-sans-serif text-right text-2xs text-gray-400"
               }
             >
-            {moment().format("YYYY-MM-DD HH:mm")}
+              {moment().format("YYYY-MM-DD HH:mm")}
             </div>
           </div>
         </div>
       </div>
-      </div>
-    )          
-}
+    </div>
+  );
+};
