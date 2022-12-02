0a6f682b0c4999fd8edfd61060c0dda2b8e80e74
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-12-02 18:36:41 +0000

    Removed style reference

--- finWidget/src/bootstrap.js
+++ finWidget/src/bootstrap.js
@@ -1,11 +1,7 @@
 import App from "./App";
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import "./styles.css";
-import StockWidget from "./StockWidget";
-
 const rootElement = document.getElementById("root");
-
 ReactDOM.render(<App />, rootElement);
 
 
