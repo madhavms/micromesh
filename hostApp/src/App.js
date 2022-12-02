24b96408e84f5a06828ce3ad0d7cda5016768bfd
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-12-02 18:40:49 +0000

    Create and pass state hook for widget style
    State hook created for widget style which is set by the widget and this style is to be injected to the shadow root where the widget is going to be attached.

--- hostApp/src/App.js
+++ hostApp/src/App.js
@@ -2,16 +2,19 @@ const StockWidget = React.lazy(() => import("finWidget/StockWidget"));
 import React, { useEffect, useRef, useState } from "react";
 import { WidgetPlaceholder } from "./components/WidgetPlaceholder";
 import "./styles.css";
+import ShadowRoot from "./utils/ShadowRoot";
 import { useFetch } from "./utils/UtilHooks";
 
 const App = () => {
   const listCache = JSON.parse(localStorage.getItem("stockList")) || [];
   const [stockList, setStockList] = useState(listCache);
+  const [widgetStyle, setWidgetStyle] = useState('')
   const [dragging, setDragging] = useState(false);
   const dragItem = useRef();
   const dragNode = useRef();
   const enteredNode = useRef();
   const allStockList = useFetch("http://localhost:8000/stocklist")
+
 
   const stockFilter = (stock) => {
     let list = stockList.map((listItem) => listItem.id);
@@ -76,6 +79,7 @@ const App = () => {
     e.preventDefault();
   };
 
+
   return (
     <div className="container">
       <h1 className="text-center">Host Application</h1>
@@ -91,19 +95,25 @@ const App = () => {
       </select>
       &nbsp;
       {stockList.map((widget, widgetI) => (
+        
         <div
+        key={widget.id}
           draggable
           value={widget.id}
-          key={widget.id}
           onDragOver={handleDragOver}
           onDragStart={(e) => handleDragStart(e, { widgetI })}
           onDragEnter={dragging ? (e) => handleDragEnter(e, { widgetI }) : null}
           className="flex mt-5"
         >
-          <React.Suspense fallback={<WidgetPlaceholder />}>
-            <StockWidget symbol={widget.id} handleDelete={handleDelete}/>
-          </React.Suspense>
+        <React.Suspense fallback={<WidgetPlaceholder />}>
+        <ShadowRoot  id={widget.id} style={widgetStyle}>
+        <StockWidget symbol={widget.id} handleDelete={handleDelete} setWidgetStyle={setWidgetStyle} widgetStyle={widgetStyle}/>
+        </ShadowRoot>
+        </React.Suspense>
+          
         </div>
+
+        
       ))}
     </div>
   );
