82dd4c029d102e576ea8b38b47d3667213d4c52f
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-12-03 01:06:22 +0000

    Accept place holder prop to show till style loads

--- hostApp/src/utils/ShadowRoot.js
+++ hostApp/src/utils/ShadowRoot.js
@@ -7,28 +7,34 @@ import React, {
 } from "react";
 import { createPortal } from "react-dom";
 
-const ShadowRoot = ({ children, style, id }) => {
+const ShadowRoot = ({ children, style, placeholder }) => {
   const [shadowRoot, setShadowRoot] = useState(null);
   const node = useRef();
+  const [styleLoaded, setStyleLoaded] = useState(false)
 
   useEffect(() => {
     const refNode = node.current;
     setShadowRoot(refNode.attachShadow({ mode: "open" }));
+    const styleElement = document.createElement("style");
+    styleElement.innerHTML = 'div { display:none;}';
+    refNode.shadowRoot.appendChild(styleElement);
   }, []);
 
   useEffect(() => {
     if (!!style) {
+      setStyleLoaded(true)
       const refNode = node.current;
       const styleElement = document.createElement("style");
-      styleElement.innerHTML = style.innerHTML;
+      styleElement.innerHTML = style.innerHTML + 'div { display:block;}';
       refNode.shadowRoot.appendChild(styleElement);
     }
   }, [style]);
 
   return (
     <Fragment>
-      <div ref={node} />
+      <div ref={node} className='host-node'/>
       {shadowRoot && createPortal(children, shadowRoot)}
+      {!styleLoaded && placeholder}
     </Fragment>
   );
 };
