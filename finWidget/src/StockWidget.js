e9fdb9570554324c9aac43f12b92a027aebf2926
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-12-09 17:36:52 +0000

    Abstract logic to get stock stock data as custom hook

--- finWidget/src/StockWidget.js
+++ finWidget/src/StockWidget.js
@@ -4,21 +4,13 @@ import axios from "axios";
 import { ErrorWidget } from "./components/ErrorWidget";
 import Widget from "./components/Widget";
 import "./styles.css";
-import { useUniqueId } from "./utils/Hooks";
+import { useStockData, useUniqueId } from "./utils/Hooks";
 
 function StockWidget(props) {
-  const uniqueId = useUniqueId();
-  const [isError, setIsError] = useState(false);
-  const {setWidgetStyle, widgetStyle} = props;
-  const [quote, setQuote] = useState({
-    price: "--",
-    var: "--",
-    time: "--",
-  });
-  const [stock, setStock] = useState({
-    stockExchange: "N/A",
-    name: "N/A",
-  });
+
+  const {setWidgetStyle, widgetStyle, symbol} = props;
+  const {isError, quote, stock} = useStockData(symbol)
+  
 
   useEffect(() => {
     if(!!window['widget-style'] && !widgetStyle && typeof setWidgetStyle === 'function'){
@@ -26,33 +18,6 @@ function StockWidget(props) {
     }
   }, []);
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const { data } = await axios.get(
-          `http://localhost:8000/stocks/${props.symbol}?v=${uniqueId}`
-        );
-        setIsError(false);
-        if (!data || !props?.symbol) {
-          return;
-        }
-        const stockDetail = data;
-        setStock({
-          stockExchange: stockDetail.stock_exchange,
-          name: stockDetail.name,
-        });
-        setQuote({
-          price: stockDetail.last,
-          var:
-            Math.trunc(-(1 - stockDetail.last / stockDetail.open) * 10000) /
-            100,
-          time: moment(stockDetail.date).format("YYYY-MM-DD HH:mm"),
-        });
-      } catch (error) {
-        setIsError(true);
-      }
-    })();
-  }, [uniqueId]);
 
   const varColor = quote.var < 0 ? "text-red-500" : "text-green-500";
   return !!props.symbol && !isError ? (
