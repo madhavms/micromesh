f1d438cdce4f0740773891355e9bbcf2cb22748e
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-12-19 18:44:57 +0000

    Added websocket API for realtime stock price

--- finAPI/stockapi/main.py
+++ finAPI/stockapi/main.py
@@ -1,11 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 from redis_om import get_redis_connection, HashModel
 from fastapi.middleware.cors import CORSMiddleware
 from pathlib import Path
-import os
-import requests
+import websockets
 import json
 import random
+import asyncio
+import json
+import random
+
 
 from utils import global_var
 
@@ -25,7 +28,7 @@ app.add_middleware(
 
 def add_variance(stock):
     pos_or_neg = random.randint(0, 1)
-    randNum = random.uniform(1, 15)
+    randNum = random.uniform(1, 9)
     randNum = randNum if pos_or_neg == 1 else -randNum
     stock['last'] = round(stock['last'] + randNum, 2)
     return stock
@@ -62,3 +65,22 @@ def get_stock(id):
         if stock_data['id'] == id:
             return add_variance(stock_data)
     return {}
+
+
+async def generate_stock_prices(websocket, symbol):
+    while True:
+        # Generate a random stock price
+        response = load_data()
+        data_to_send = {}
+        for stock_data in response:
+            if stock_data['id'] == symbol:
+                data_to_send = add_variance(stock_data)
+        # Send the dummy stock price to the client
+        await websocket.send_text(json.dumps(data_to_send))
+        await asyncio.sleep(3)
+
+@app.websocket("/stockprices/{symbol}")
+async def websocket_endpoint(websocket: WebSocket, symbol: str):
+    await websocket.accept()
+    await generate_stock_prices(websocket, symbol)
+
