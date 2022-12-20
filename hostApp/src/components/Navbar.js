b402ef32794c701761d98c5c2898332f81897958
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-12-20 15:22:42 +0000

    Create navbar component

---
+++ hostApp/src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: 'center',
+    color: 'white',
+  },
+}));
+
+export default function Navbar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Host Application
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
