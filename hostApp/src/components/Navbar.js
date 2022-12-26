4ab256692cb9e04aab22532e708ead307858fe35
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-12-26 10:35:07 +0000

    Support for dark and light mode

--- hostApp/src/components/Navbar.js
+++ hostApp/src/components/Navbar.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import Brightness4Icon from "@material-ui/icons/Brightness4";
+import Brightness7Icon from "@material-ui/icons/Brightness7";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,18 +19,32 @@ const useStyles = makeStyles((theme) => ({
     textAlign: "center",
     color: "white",
   },
+  appBar: {
+    position: 'fixed',
+    top: 0,
+    width: '100%',
+    zIndex: 10,
+    backgroundColor: (props) => (props.mode === "light" ? "#7b68ee" : "#212121"),
+  },
 }));
 
-export default function Navbar() {
-  const classes = useStyles();
+export default function Navbar({mode, setMode}) {
+  const toggleMode = () => {
+    setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+  };
+
+  const classes = useStyles({ mode });
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.appBar}>
         <Toolbar>
           <Typography variant="h6" className={classes.title}>
-            Host Application
+            Remote Application
           </Typography>
+          <IconButton onClick={toggleMode}>
+            {mode === "light" ? <Brightness7Icon /> : <Brightness4Icon />}
+          </IconButton>
         </Toolbar>
       </AppBar>
     </div>
