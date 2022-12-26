fb0f104700293f608c5cc9eea1343a996a827b8e
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-12-26 10:35:46 +0000

    remove unused imports

--- hostApp/src/components/DragNDrop.js
+++ hostApp/src/components/DragNDrop.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useContext } from "react";
+import React, {useContext } from "react";
 import { DragContext } from "../utils/DragContext";
 
 export const DragNDrop = ({ children, id, widgetI, setDisplayStockList }) => {
