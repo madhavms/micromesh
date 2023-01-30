b58c24072d5aa8ad2e8e351aff15adc3afabf5b0
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-01-30 21:33:56 +0000

    Create RiskWidget.js

---
+++ finWidget/src/RiskWidget.js
@@ -0,0 +1,52 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { useStockRisk } from './utils/Hooks';
+import {subscribe, unsubscribe} from "messagebusmono";
+import RiskWidgetPlaceholder from './components/RiskWidgetPlaceholder';
+
+function StockRiskWidget({symbol} = null) {
+
+  const [currentSymbol, setCurrentSymbol] = useState("");
+  const { riskData, isLoading, error } = useStockRisk(currentSymbol);
+
+  const handleMessage = (event) => {
+    console
+    if(event.data.message && event.data.message.symbol)
+    setCurrentSymbol(event.data.message.symbol);
+  };
+
+  useEffect(() => {
+    subscribe(handleMessage);
+    return(() => {
+      unsubscribe(handleMessage);
+    })
+  },[]);
+
+  useEffect(() => {
+    if(!!symbol) {
+      setCurrentSymbol(symbol);
+    }
+  }, [symbol]);
+
+
+
+  if (isLoading) {
+    return <RiskWidgetPlaceholder />;
+  }
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+  if (!riskData || !currentSymbol) {
+    return <div>Enter a symbol to see the risk data</div>;
+  }
+
+
+  return (
+    <div className="risk-container">
+      <div className="risk-symbol">{riskData.id}</div>
+      <div className="risk-risk">Risk: {riskData.risk}</div>
+      <div className="risk-description">{riskData.description}</div>
+    </div>
+  );
+}
+
+export default StockRiskWidget;
