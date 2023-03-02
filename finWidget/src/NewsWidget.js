79b24aa6403aff5505a7624213b980971a49a420
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-03-02 18:06:46 +0000

    Update NewsWidget.js

--- finWidget/src/NewsWidget.js
+++ finWidget/src/NewsWidget.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "./css/DynamicFinancialNewsWidget.css";
 
-const DynamicFinancialNewsWidget = () => {
+const DynamicFinancialNewsWidget = (props) => {
   const [news, setNews] = useState([]);
+
 
   const newsList = [
     {
