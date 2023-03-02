9b95d87c402dbf4dcf7980c4f87cb15ee3090679
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-03-02 17:46:42 +0000

    Create StockRiskWidget.js

---
+++ finWidget/src/StockRiskWidget.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import StockWidget from "./StockWidget";
+import RiskWidget from "./RiskWidget";
+
+function StockRiskWidget(props) {
+  const { setWidgetStyle, widgetStyle, handleClose } = props;
+
+  useEffect(() => {
+    if (
+      !!window["widget-style"] &&
+      !widgetStyle &&
+      typeof setWidgetStyle === "function"
+    ) {
+      setWidgetStyle(window["widget-style"]);
+    }
+  }, []);
+
+  return (
+<div style={{ display: 'flex', flexDirection: 'row' }}>
+  <StockWidget
+    symbol="AAPL"
+    handleClose={handleClose}
+  />
+  <RiskWidget />
+</div>
+
+  );
+}
+
+export default StockRiskWidget;
