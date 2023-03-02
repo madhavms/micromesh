289494d6927b1a8e5d533c8aba5574c3b8e97b6c
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-03-02 17:46:52 +0000

    Update Navbar.js

--- hostApp/src/components/Navbar.js
+++ hostApp/src/components/Navbar.js
@@ -6,6 +6,13 @@ import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import Brightness4Icon from "@material-ui/icons/Brightness4";
 import Brightness7Icon from "@material-ui/icons/Brightness7";
+import MenuIcon from "@material-ui/icons/Menu";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,21 +34,41 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: (props) =>
       props.mode === "light" ? "#7b68ee" : "#212121",
   },
+  drawer: {
+    width: drawerWidth,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
 }));
 
-export default function Navbar({ mode, setMode }) {
+export default function Navbar({ mode, setMode, menu=[], handleMenuSelection }) {
+  console.log("menu", menu);
   const toggleMode = () => {
     const newMode = mode === "light" ? "dark" : "light";
     setMode(newMode);
     localStorage.setItem("mode", newMode);
   };
 
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
   const classes = useStyles({ mode });
+
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!drawerOpen);
+  };
 
   return (
     <div className={classes.root}>
       <AppBar position="static" className={classes.appBar}>
         <Toolbar>
+          <IconButton
+            onClick={handleDrawerToggle}
+            edge="start"
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
           <Typography variant="h6" className={classes.title}>
             Host Application
           </Typography>
@@ -50,6 +77,22 @@ export default function Navbar({ mode, setMode }) {
           </IconButton>
         </Toolbar>
       </AppBar>
+      <Drawer
+        className={classes.drawer}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        open={drawerOpen}
+        onClose={handleDrawerToggle}
+      >
+        <List>
+        {menu.length !==0 && menu.map((item) => (
+          <ListItem key={item.appId} button onClick={(e) => handleMenuSelection(e, item.appId)}>
+            <ListItemText primary={item.label} />
+          </ListItem>
+        ))}
+        </List>
+      </Drawer>
     </div>
   );
 }
