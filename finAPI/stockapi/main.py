88aa037b1656185328cdf8d7748d365c6c85508e
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-03-03 13:13:06 +0000

    API for widget onboarding

--- finAPI/stockapi/main.py
+++ finAPI/stockapi/main.py
@@ -8,10 +8,19 @@ import json
 import random
 from utils import global_var
 from dotenv import load_dotenv, find_dotenv
+from pydantic import BaseModel
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 load_dotenv(find_dotenv())
 
 app = FastAPI()
+
+# create SQLite database
+engine = create_engine("sqlite:///widgets.db")
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 
 app.add_middleware(
     CORSMiddleware,
@@ -46,15 +55,88 @@ def load_risk_data():
         return json.load(f)
     
 
-@app.get("/widgets")
-async def get_widgets():
-    widgets = load_apps_data()
+
+# define Widget model
+class Widget(Base):
+    __tablename__ = "widgets"
+    id = Column(Integer, primary_key=True, index=True)
+    url = Column(String, index=True)
+    appId = Column(String, index=True)
+    remoteId = Column(String, index=True)
+
+# define MenuItem model
+class MenuItem(Base):
+    __tablename__ = "menu_items"
+    id = Column(Integer, primary_key=True, index=True)
+    appId = Column(String, index=True)
+    label = Column(String, index=True)
+
+# create database tables
+Base.metadata.create_all(bind=engine)
+
+# define request model
+class WidgetCreate(BaseModel):
+    url: str
+    appId: str
+    remoteId: str
+
+# define endpoints
+@app.post("/widgets/")
+async def create_widget(widget: WidgetCreate):
+    db = SessionLocal()
+    db_widget = Widget(url=widget.url, appId=widget.appId, remoteId=widget.remoteId)
+    db.add(db_widget)
+    db.commit()
+    db.refresh(db_widget)
+    return db_widget
+
+@app.get("/widgets/")
+async def read_widgets():
+    db = SessionLocal()
+    widgets = db.query(Widget).all()
     return widgets
 
-@app.get("/menu")
-async def get_menu():
-    widgets = load_menu_data()
-    return widgets
+@app.delete("/widgets/{widget_id}")
+async def delete_widget(widget_id: int):
+    db = SessionLocal()
+    widget = db.query(Widget).filter(Widget.id == widget_id).first()
+    if widget:
+        db.delete(widget)
+        db.commit()
+        return {"message": "Widget deleted successfully"}
+    return {"error": "Widget not found"}
+
+
+# define request model
+class MenuItemCreate(BaseModel):
+    appId: str
+    label: str
+
+# define endpoints
+@app.post("/menu/")
+async def create_menu_item(menu_item: MenuItemCreate):
+    db = SessionLocal()
+    db_menu_item = MenuItem(appId=menu_item.appId, label=menu_item.label)
+    db.add(db_menu_item)
+    db.commit()
+    db.refresh(db_menu_item)
+    return db_menu_item
+
+@app.get("/menu/")
+async def read_menu_items():
+    db = SessionLocal()
+    menu_items = db.query(MenuItem).all()
+    return menu_items
+
+@app.delete("/menu/{menu_item_id}")
+async def delete_menu_item(menu_item_id: int):
+    db = SessionLocal()
+    menu_item = db.query(MenuItem).filter(MenuItem.id == menu_item_id).first()
+    if menu_item:
+        db.delete(menu_item)
+        db.commit()
+        return {"message": "Menu item deleted successfully"}
+    return {"error": "Menu item not found"}
 
 @app.get("/")
 async def root():
