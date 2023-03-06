13abaf29e95153a59334a125015bf062da747d62
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-03-06 17:32:02 +0000

    Update bootstrap.js

--- hostApp/src/bootstrap.js
+++ hostApp/src/bootstrap.js
@@ -1,19 +1,13 @@
-import NewApp from "./NewApp";
 import React, { StrictMode, useState } from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 import { DragContextProvider } from "./utils/DragContext";
+import App from "./App";
 
-
-    const rootElement = document.getElementById("root");
-    ReactDOM.render(
-      <DragContextProvider>
-        <NewApp />
-      </DragContextProvider>,
-      rootElement
-    )
-
-
-
-
-
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <DragContextProvider>
+    <App/>
+  </DragContextProvider>,
+  rootElement
+);
