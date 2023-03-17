c6759ecbf874e2602b732f7203ea23721bc0095d
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-03-17 21:55:18 +0000

    Docker build update to remove pulling source code

--- hostApp/webpack.config.js
+++ hostApp/webpack.config.js
@@ -1,10 +1,20 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { ModuleFederationPlugin } = require("webpack").container;
 const path = require("path");
+const dotenv = require('dotenv');
+const webpack = require('webpack');
 
-const lookupRemoteEntryUrl = (port) => {
-  return `//localhost:${port}/widgetRemote.js`;
-};
+
+// call dotenv and it will return an Object with a parsed key 
+const env = dotenv.config().parsed;
+
+// reduce it to a nice object, the same as before
+const envKeys = Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+  return prev;
+}, {});
+
+console.log("env keys=",envKeys)
 
 module.exports = {
   entry: "./src/index",
@@ -54,5 +64,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
+    new webpack.DefinePlugin(envKeys),
   ],
 };
