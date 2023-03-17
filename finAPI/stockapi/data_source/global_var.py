680983c23cf0c923627d9c5320981eacbf118b46
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-03-17 13:01:43 +0000

    Changed folder name

---
+++ finAPI/stockapi/data_source/global_var.py
@@ -0,0 +1,4 @@
+STOCK_DATA_PATH = './data_source/stock_data.json'
+RISK_DATA_PATH = './data_source/risk_data.json'
+MENU_DATA_PATH = './data_source/menu_data.json'
+APPS_DATA_PATH = './data_source/apps_data.json'
