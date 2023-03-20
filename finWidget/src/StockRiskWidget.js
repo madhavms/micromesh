eb43c1c4a12aac491cb003126800495f159f825f
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-03-20 00:20:54 +0000

    Added media query to to change flex direction if less space

--- finWidget/src/StockRiskWidget.js
+++ finWidget/src/StockRiskWidget.js
@@ -4,30 +4,40 @@ import RiskWidget from "./RiskWidget";
 
 function StockRiskWidget(props) {
   const { setWidgetStyle, widgetStyle, handleClose, uuid } = props;
+
   useEffect(() => {
-    if (
-      !!window["widget-style"] &&
-      
-      typeof setWidgetStyle === "function"
-    ) {
+    if (!!window["widget-style"] && typeof setWidgetStyle === "function") {
       setWidgetStyle(window["widget-style"]);
     }
   }, []);
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'row'}}>
-    <div style={{ flexGrow: 0 }}>
-      <StockWidget
-        symbol="AAPL"
-        handleClose={handleClose}
-        uuid={uuid}
-      />
-    </div>
-    <div style={{ flexGrow: 0 }}>
-      <RiskWidget symbol="AAPL" uuid={uuid}/>
-    </div>
-  </div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "center",
+      }}
+    >
+      <div style={{ flexGrow: 0 }}>
+        <StockWidget symbol="AAPL" handleClose={handleClose} uuid={uuid} />
+      </div>
+      <div style={{ flexGrow: 0 }}>
+        <RiskWidget symbol="AAPL" uuid={uuid} />
+      </div>
 
+      {/* Add media query to change flex-direction to column for small screens */}
+      <style>
+        {`
+          @media screen and (max-width: 768px) {
+            div {
+              flex-direction: column;
+            }
+          }
+        `}
+      </style>
+    </div>
   );
 }
 
