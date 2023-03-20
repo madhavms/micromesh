19b071862b351bc9eca56a0b2a75ba0caebd64d3
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-03-20 14:41:04 +0000

    Added other links to navbar

--- hostApp/src/components/Navbar.js
+++ hostApp/src/components/Navbar.js
@@ -11,12 +11,18 @@ import Drawer from "@material-ui/core/Drawer";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+  },
+  otherLinks: {
+    color: "white",
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -50,12 +56,22 @@ export default function Navbar({ mode, setMode, menu=[], handleMenuSelection }) 
   };
 
   const [drawerOpen, setDrawerOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
 
   const classes = useStyles({ mode });
 
   const handleDrawerToggle = () => {
     setDrawerOpen(!drawerOpen);
   };
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+  
 
   return (
     <div className={classes.root}>
@@ -68,12 +84,43 @@ export default function Navbar({ mode, setMode, menu=[], handleMenuSelection }) 
           >
             <MenuIcon />
           </IconButton>
+          <Typography variant="subtitle1" className={classes.menuTitle}>
+            Menu
+          </Typography>
           <Typography variant="h6" className={classes.title}>
             Host Application
           </Typography>
           <IconButton onClick={toggleMode}>
             {mode === "light" ? <Brightness7Icon /> : <Brightness4Icon />}
           </IconButton>
+          <IconButton onClick={handleMenuOpen}>
+            <Typography variant="subtitle1" className={classes.otherLinks}>Related Apps</Typography>
+          </IconButton>
+          <Menu
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleMenuClose}
+          >
+            <MenuItem
+              onClick={handleMenuClose}
+              component="a"
+              href={process.env.SHOWCASE_URL}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Showcase App
+            </MenuItem>
+            <MenuItem
+              onClick={handleMenuClose}
+              component="a"
+              href={process.env.ONBOARDING_URL}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Onboarding App
+            </MenuItem>
+          </Menu>
         </Toolbar>
       </AppBar>
       <Drawer
