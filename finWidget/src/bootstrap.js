57312d3b6995e1a62d696d6b3e2cef283d45b065
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-03-22 09:48:43 +0000

    Migrated the finWIdget project to separate repo

--- finWidget/src/bootstrap.js
+++
@@ -1,5 +0,0 @@
-import App from "./App";
-import React, { StrictMode } from "react";
-import ReactDOM from "react-dom";
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
