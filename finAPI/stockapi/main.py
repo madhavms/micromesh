936d813ec144a825454845ea940b078b2cecccd0
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-03-25 23:26:33 +0000

    Use mongoDB instead of sql lite for storage

--- finAPI/stockapi/main.py
+++ finAPI/stockapi/main.py
@@ -6,14 +6,12 @@ import requests
 from pathlib import Path
 import json
 import random
+import os
 import asyncio
 import json
-import random
 from dotenv import load_dotenv, find_dotenv
 from pydantic import BaseModel
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from pymongo import MongoClient
 
 STOCK_DATA_PATH = 'https://raw.githubusercontent.com/madhavms/react-host-remote/main/finAPI/stockapi/data_source/stock_data.json'
 RISK_DATA_PATH = 'https://raw.githubusercontent.com/madhavms/react-host-remote/main/finAPI/stockapi/data_source/risk_data.json'
@@ -24,10 +22,12 @@ load_dotenv(find_dotenv())
 
 app = FastAPI()
 
-# create SQLite database
-engine = create_engine("sqlite:///widgets.db")
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+# retrieve MongoDB Atlas connection string from environment variable
+mongo_client_address = os.environ.get('MONGO_CLIENT_ADDRESS')
+
+# create MongoDB client and database
+client = MongoClient(mongo_client_address)
+db = client['micromeshdb']
 
 app.add_middleware(
     CORSMiddleware,
@@ -55,22 +55,16 @@ def load_risk_data():
 
 
 # define Widget model
-class Widget(Base):
-    __tablename__ = "widget_items"
-    id = Column(Integer, primary_key=True, index=True)
-    url = Column(String, index=True)
-    appId = Column(String, index=True)
-    remoteId = Column(String, index=True)
+class Widget(BaseModel):
+    url: str
+    appId: str
+    remoteId: str
 
 # define MenuItem model
-class MenuItem(Base):
-    __tablename__ = "menu_items"
-    id = Column(Integer, primary_key=True, index=True)
-    appId = Column(String, index=True)
-    label = Column(String, index=True)
+class MenuItem(BaseModel):
+    appId: str
+    label: str
 
-# create database tables
-Base.metadata.create_all(bind=engine)
 
 # define request model
 class WidgetCreate(BaseModel):
@@ -78,87 +72,74 @@ class WidgetCreate(BaseModel):
     appId: str
     remoteId: str
 
-# define endpoints
-@app.post("/widgets/")
-async def create_widget(widget: WidgetCreate):
-    db = SessionLocal()
-    db_widget = Widget(url=widget.url, appId=widget.appId, remoteId=widget.remoteId)
-    db.add(db_widget)
-    db.commit()
-    db.refresh(db_widget)
-    return db_widget
-
-@app.get("/widgets/")
-async def read_widgets():
-    db = SessionLocal()
-    widgets = db.query(Widget).all()
-    return widgets
-
-@app.delete("/widgets/{widget_id}")
-async def delete_widget(widget_id: int):
-    db = SessionLocal()
-    widget = db.query(Widget).filter(Widget.id == widget_id).first()
-    if widget:
-        db.delete(widget)
-        db.commit()
-        return {"message": "Widget deleted successfully"}
-    return {"error": "Widget not found"}
-
-
-@app.put("/widgets/{widget_id}")
-async def update_widget(widget_id: int, widget):
-    db = SessionLocal()
-    db_widget = db.query(Widget).filter(Widget.id == widget_id).first()
-    if not db_widget:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Widget not found")
-    for field, value in widget.dict(exclude_unset=True).items():
-        setattr(db_widget, field, value)
-    db.commit()
-    db.refresh(db_widget)
-    return db_widget
-
 
 # define request model
 class MenuItemCreate(BaseModel):
     appId: str
     label: str
 
+
+# define endpoints
+@app.post("/widgets/")
+async def create_widget(widget: WidgetCreate):
+    widget_dict = widget.dict()
+    result = db.widgets.insert_one(widget_dict)
+    widget_dict['_id'] = str(result.inserted_id)
+    return widget_dict
+
+@app.get("/widgets/")
+async def read_widgets():
+    widgets = list(db.widgets.find())
+    for widget in widgets:
+        widget['_id'] = str(widget['_id'])
+    return widgets
+
+@app.delete("/widgets/{widget_id}")
+async def delete_widget(widget_id: str):
+    result = db.widgets.delete_one({'_id': widget_id})
+    if result.deleted_count > 0:
+        return {"message": "Widget deleted successfully"}
+    return {"error": "Widget not found"}
+
+
+@app.put("/widgets/{widget_id}")
+async def update_widget(widget_id: str, widget: Widget):
+    widget_dict = widget.dict(exclude_unset=True)
+    result = db.widgets.update_one({'_id': widget_id}, {'$set': widget_dict})
+    if result.modified_count > 0:
+        widget_dict['_id'] = widget_id
+        return widget_dict
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Widget not found")
+
+
 # define endpoints
 @app.post("/menu/")
 async def create_menu_item(menu_item: MenuItemCreate):
-    db = SessionLocal()
-    db_menu_item = MenuItem(appId=menu_item.appId, label=menu_item.label)
-    db.add(db_menu_item)
-    db.commit()
-    db.refresh(db_menu_item)
-    return db_menu_item
+    menu_item_dict = menu_item.dict()
+    result = db.menu    .insert_one(menu_item_dict)
+    menu_item_dict['_id'] = str(result.inserted_id)
+    return menu_item_dict
 
 @app.put("/menu/{item_id}")
-async def update_menu_item(item_id: int, menu_item):
-    db = SessionLocal()
-    db_menu_item = db.query(MenuItem).filter(MenuItem.id == item_id).first()
-    if not db_menu_item:
-        raise HTTPException(status_code=404, detail="Menu item not found")
-    for field, value in menu_item.dict(exclude_unset=True).items():
-        setattr(db_menu_item, field, value)
-    db.commit()
-    db.refresh(db_menu_item)
-    return db_menu_item
-
+async def update_menu_item(item_id: str, menu_item: MenuItem):
+    menu_item_dict = menu_item.dict(exclude_unset=True)
+    result = db.menu.update_one({'_id': item_id}, {'$set': menu_item_dict})
+    if result.modified_count > 0:
+        menu_item_dict['_id'] = item_id
+        return menu_item_dict
+    raise HTTPException(status_code=404, detail="Menu item not found")
 
 @app.get("/menu/")
 async def read_menu_items():
-    db = SessionLocal()
-    menu_items = db.query(MenuItem).all()
+    menu_items = list(db.menu.find())
+    for menu_item in menu_items:
+        menu_item['_id'] = str(menu_item['_id'])
     return menu_items
 
 @app.delete("/menu/{menu_item_id}")
-async def delete_menu_item(menu_item_id: int):
-    db = SessionLocal()
-    menu_item = db.query(MenuItem).filter(MenuItem.id == menu_item_id).first()
-    if menu_item:
-        db.delete(menu_item)
-        db.commit()
+async def delete_menu_item(menu_item_id: str):
+    result = db.menu.delete_one({'_id': menu_item_id})
+    if result.deleted_count > 0:
         return {"message": "Menu item deleted successfully"}
     return {"error": "Menu item not found"}
 
@@ -215,13 +196,4 @@ async def generate_stock_prices(websocket, symbol):
 async def websocket_endpoint(websocket: WebSocket, symbol: str):
     await websocket.accept()
     await generate_stock_prices(websocket, symbol)
-
-@app.get("/.well-known/pki-validation/0D53200C71F63CDD63C015997A9A691E.txt")
-async def serve_validation_file():
-    url = "https://raw.githubusercontent.com/madhavms/react-host-remote/main/finAPI/stockapi/.well-known/pki-validation/0D53200C71F63CDD63C015997A9A691E.txt"
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            content = await response.content.read()
-            return Response(content, media_type="text/plain")
-
 
