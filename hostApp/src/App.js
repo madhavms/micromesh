dfbc0e811c9c93c278f13de52bb5e03efc3d3847
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-04-01 14:46:04 +0100

    All routes show NewApp

--- hostApp/src/App.js
+++ hostApp/src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/micromesh" element={<NewApp />} />
+      <Route path="*" element={<NewApp />} />
       </Routes>
     </BrowserRouter>
   );
