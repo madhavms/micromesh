79bdeeea5c5629a11631893463ebaf89d1a6d237
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-04-04 17:27:50 +0100

    Update AboutScreen.js

--- hostApp/src/components/AboutScreen.js
+++ hostApp/src/components/AboutScreen.js
@@ -7,7 +7,8 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: (props) => (props.mode === "dark" ? "#132f4c" : "#FAFAFA"),
     color: (props) => (props.mode === "dark" ? "#FFFFFF" : "#333333"),
     padding: theme.spacing(3),
-    boxShadow: "0px 1px 3px rgba(0, 0, 0, 0.1), 0px 1px 2px rgba(0, 0, 0, 0.06)",
+    boxShadow:
+      "0px 1px 3px rgba(0, 0, 0, 0.1), 0px 1px 2px rgba(0, 0, 0, 0.06)",
     transition: "box-shadow 0.4s ease-in-out",
     border: "none",
   },
@@ -44,15 +45,19 @@ const About = ({ mode }) => {
         </Typography>
         <Box className={classes.textContainer}>
           <Typography variant="body1" className={classes.text}>
-            This is a micro-frontend platform that performs run-time discovery,
-            loading and orchestration of independently deployed applications
-            bundled in micro-frontend compatible format. 
+            This platform is a micro-frontend solution that allows real-time
+            discovery, loading, and orchestration of independently deployed
+            applications in a micro-frontend compatible format.
           </Typography>
           <Box mt={2}>
             <Typography variant="body1" className={classes.text}>
-              To access the microfrontend widgets, please use the global
-              discovery menu on the left-hand. The MicroMesh platform provides a global disovery menu which helps the users to access onboarded microfrontend applications.
-              The platform also introduces an enhanced workspace feature that utilizes a tabbed navigation system, providing users with a seamless and organized way to access multiple applications simultaneously.
+              To access the microfrontend widgets, use the global discovery menu
+              on the left-hand side. The MicroMesh platform provides a
+              user-friendly global discovery menu to facilitate access to
+              onboarded microfrontend applications. Additionally, the platform
+              incorporates a practical workspace feature with a tabbed
+              navigation system, enabling users to interact with multiple
+              applications concurrently in an organized manner.
             </Typography>
           </Box>
         </Box>
