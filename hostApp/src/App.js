0d8c196a8ead907e68b05795a70e3ae56ef4ba9b
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-06-02 01:17:53 +0100

    pass fn for toggling dark mode

--- hostApp/src/App.js
+++ hostApp/src/App.js
@@ -2,12 +2,12 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Shell from "./Shell";
 
-const App = ({apps, menu, mode, setMode}) => {
+const App = ({apps, menu, toggleMode, mode}) => {
 
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="*" element={<Shell apps={apps} menu={menu} mode={mode} setMode={setMode}/>} />
+      <Route path="*" element={<Shell apps={apps} menu={menu} toggleMode={toggleMode} mode={mode}/>} />
       </Routes>
     </BrowserRouter>
   );
